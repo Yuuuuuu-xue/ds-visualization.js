55830b5d3eaa30c3335e070f4b28c32a0b3f2f52
Yuuuuuu-xue <yuxue010625@gmail.com>
2021-11-15 14:30:16 -0500

    Add examples of how to draw circles with canvas and  svg

---
+++ test.js
@@ -0,0 +1,14 @@
+const c = document.getElementById("myId");
+const ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+
+document.getElementById("bluecircle").addEventListener('click', () => {
+  console.log('click');
+  document.getElementById('line').style.stroke = 'red';
+});
+
+document.getElementById("svg").insertAdjacentHTML('beforeend', `<circle cx="220" cy="300" r="50" stroke="crimson" fill="none" />`);
