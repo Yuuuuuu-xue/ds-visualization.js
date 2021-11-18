49ac7b4ba10edb8324eb0e09cd8d27a89ad58c2b
Yuuuuuu-xue <yuxue010625@gmail.com>
2021-11-18 18:48:28 -0500

    Add arrows and exampels

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ $(document).ready(function() {
   dsController.pushEdge('1', '3');
   dsController.pushEdge('2', '1');
   dsController.pushEdge('4', '3');
+  dsController.pushEdge('1', '2');
   console.log(dsController.getCurrentGraphInfo());
 });
