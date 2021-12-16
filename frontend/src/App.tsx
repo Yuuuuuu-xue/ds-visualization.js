b7b3e3f1c635dd4cf1cc5d9988dc6fd670abbbff
Yuuuuuu-xue <yuxue010625@gmail.com>
2021-12-16 17:34:18 -0500

    Ready to build the landing page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 function App() {
   const ds = (window as any).ds;
-  let dataStructure = new ds.DataStructureController(800, 400, document.getElementById("test"));
+  let dsController = new ds.DataStructureController(800, 400);
+
+  useEffect(() => {
+    document.getElementById('app')?.insertAdjacentElement('beforeend', dsController.getCanvasElement());
+  }, [])
 
   return (
-    <div className="App">
-      Hello World
-      <div id='test'></div>
+    <div className="App" id='app'>
     </div>
   );
 }
