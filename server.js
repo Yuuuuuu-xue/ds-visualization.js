b44b963e1a9cebcd17941254939aeadd427bd83e
Yuuuuuu-xue <yuxue010625@gmail.com>
2021-12-16 23:57:37 -0500

    Build frontend as static files

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 5000;
 app.use(morgan("dev"));
 
 // static directory
-app.use(express.static(path.join(__dirname, '/public')));
-
-
-app.get('/', (req, res) => {
-  res.status(200).send("DS.js library");
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, './frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`);
