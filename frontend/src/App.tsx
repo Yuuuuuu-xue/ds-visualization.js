9776126ac117d5669c0952c32e038e781f2df257
Yuuuuuu-xue <yuxue010625@gmail.com>
2021-12-16 23:02:42 -0500

    Set up the home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home/Home';
 
 function App() {
-  const ds = (window as any).ds;
-  let dsController = new ds.DataStructureController(800, 400);
+  // const ds = (window as any).ds;
+  // let dsController = new ds.DataStructureController(800, 400);
 
-  useEffect(() => {
-    document.getElementById('app')?.insertAdjacentElement('beforeend', dsController.getCanvasElement());
-  }, [])
+  // useEffect(() => {
+  //   document.getElementById('app')?.insertAdjacentElement('beforeend', dsController.getCanvasElement());
+  // }, [])
 
   return (
-    <div className="App" id='app'>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/Pages/Home/Home.tsx
@@ -0,0 +1,57 @@
+import { FC, ReactElement } from "react";
+import Button from '@mui/material/Button';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { styled } from "@mui/material/styles";
+import './Home.scss';
+import Grid from '@mui/material/Grid';
+
+interface Props {
+
+};
+
+const ColorContainedButton = styled(Button)(({ theme }) => ({
+  
+  backgroundColor: '#EC3257',
+  '&:hover': {
+    backgroundColor: '#DC143C',
+  },
+}));
+
+const ColorOutlinedButton = styled(Button)(({ theme }) => ({
+  color: '#EC3257',
+  borderColor: '#EC3257',
+  '&:hover': {
+    borderColor: '#EC3257'
+  }
+}))
+
+
+const Home: FC<Props> = (): ReactElement => {
+  return (
+    <div className="app">
+      <h1 className="title">DS.js</h1>
+      <ColorOutlinedButton className="source-code-button" variant="outlined" size="large">
+        Source Code
+        &nbsp;&nbsp;
+        <GitHubIcon />
+      </ColorOutlinedButton>
+      <p className="description">
+        A library that helps developers to draw and create interactions for end-users visualizing data structures. 
+      </p>
+      <Grid container spacing={2}>
+        <Grid xs={4} sx={{textAlign: "center"}}>
+          <ColorContainedButton variant="contained" size="large">Get Started <NavigateNextIcon /></ColorContainedButton>
+        </Grid>
+        <Grid xs={4} sx={{textAlign: "center"}}>
+          <ColorOutlinedButton variant="outlined" size="large">Documentation</ColorOutlinedButton>
+        </Grid>
+        <Grid xs={4} sx={{textAlign: "center"}}>
+          <ColorContainedButton variant="contained" size="large">Demo</ColorContainedButton>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Home;
